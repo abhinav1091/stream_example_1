cf0f4c1a5d06c2a088d898b56813ac8ffbdca910
akumar <abhinav_1091@yahoo.com>
2018-10-29 19:27:07 +0530

    Merge branch 'master' into secondBranch
    # Conflicts:
    #	src/main/scala/twitter_tweet.scala

--- src/main/scala/append_log_reader.scala
+++ src/main/scala/append_log_reader.scala
@@ -2,13 +2,20 @@
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.streaming.{Seconds, StreamingContext}
+import java.util
+
+import java.util.Properties
+
+import scala.collection.JavaConverters._
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.common.serialization.StringDeserializer
 
 object append_log_reader {
 
-  def main( args: Array[String]): Unit = {
+  def main(args: Array[String]): Unit = {
 
 
-    val conf = new SparkConf().setAppName("stream").setMaster("local[2]")
+    /* val conf = new SparkConf().setAppName("stream").setMaster("local[2]")
     //val session = SparkSession.builder().appName("word_counter").master("local").getOrCreate()
     val ssc = new StreamingContext(conf, Seconds(3))
 
@@ -27,5 +34,23 @@ object append_log_reader {
     data.print()
     ssc.start()
     ssc.awaitTermination()
+  }*/
+
+    val properties = new Properties()
+    properties.put("bootstrap.servers", "localhost:9092")
+   // properties.put("group.id", "consumer-tutorial")
+    properties.put("key.deserializer", classOf[StringDeserializer])
+    properties.put("value.deserializer", classOf[StringDeserializer])
+
+    val kafkaConsumer = new KafkaConsumer[String, String](properties)
+    kafkaConsumer.subscribe("test")
+
+    while (true) {
+      val results = kafkaConsumer.poll(2000).asScala
+      for ((topic, data) <- results) {
+        // Do stuff
+        println(data)
+      }
+    }
   }
 }
