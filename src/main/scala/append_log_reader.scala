0851bd26bd68f0e1771aff482fcfbbec739df3b2
akumar <abhinav_1091@yahoo.com>
2018-10-30 22:52:01 +0530

    Merge branch 'master' into secondBranch
    # Conflicts:
    #	src/main/scala/twitter_tweet.scala

--- src/main/scala/append_log_reader.scala
+++ src/main/scala/append_log_reader.scala
@@ -1,57 +1,37 @@
 
-import org.apache.spark.SparkConf
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.streaming.{Seconds, StreamingContext}
-import java.util
+import java.util.{Collections, Properties}
 
-import java.util.Properties
-
-import scala.collection.JavaConverters._
+import scala.collection.JavaConversions._
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.kafka.common.serialization.StringDeserializer
-
+import org.apache.kafka.clients.consumer.ConsumerRecords
 object append_log_reader {
 
-  def main(args: Array[String]): Unit = {
-
-
-    /* val conf = new SparkConf().setAppName("stream").setMaster("local[2]")
-    //val session = SparkSession.builder().appName("word_counter").master("local").getOrCreate()
-    val ssc = new StreamingContext(conf, Seconds(3))
-
-    val data = ssc.textFileStream("/Users/abhinavkumar/Documents/stream/")
-    data.print()
-
-    /* val res= data.foreachRDD(t=>
-    {
-      val test = t.map(line=> line.toString)
-    }
-    )*/
-
-
-
-    // res.print()
-    data.print()
-    ssc.start()
-    ssc.awaitTermination()
-  }*/
+  def main(args: Array[String]) :Unit = {
 
     val properties = new Properties()
 
     properties.put("bootstrap.servers", "localhost:9092")
-   // properties.put("group.id", "consumer-tutorial")
+    properties.put("group.id", "consumer-tutorial")
     properties.put("key.deserializer", classOf[StringDeserializer])
     properties.put("value.deserializer", classOf[StringDeserializer])
+    //properties.put("partition.assignment.strategy", "range")
 
-    val kafkaConsumer = new KafkaConsumer[String, String](properties)
-    kafkaConsumer.subscribe("test")
+    val kafkaConsumer = new KafkaConsumer(properties)
+    kafkaConsumer.subscribe(Collections.singletonList("test2"))
 
     while (true) {
-      val results = kafkaConsumer.poll(2000).asScala
-      for ((topic, data) <- results) {
-        // Do stuff
-        println(data)
-      }
+      val results: ConsumerRecords[String, String] = kafkaConsumer.poll(5000).asInstanceOf[ConsumerRecords[String, String]]
+
+      //println(results.count())
+     for(  record <- results )
+        {
+          if (record.value().split(" ").contains("time=0.095")) {
+            println(s"offset = ${record.offset()}, key = ${record.key()}, value = ${record.value()}")
+            record.value().split(" ").map(x=> println(x))
+          }
+        }
     }
+
   }
 }
